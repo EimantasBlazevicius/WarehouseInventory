f71764caff36ce38be68b8ade39dd23ed10b61a3
EimantasBlazevicius <eimantas.blazevicius@bazaarvoice.com>
2021-08-06 16:07:09 +0300

    Student part done

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,10 +1,45 @@
 import { useParams } from "react-router-dom";
 
+import React, { useState, useContext, useEffect } from "react";
+import ProductsContext from "../../context/ProductsContext";
+
 export interface ProductProps {}
 
 const Product: React.FC<ProductProps> = () => {
+  const [name, setName] = useState<string>("");
+  const [ean, setEan] = useState<string>("");
+  const [type, setType] = useState<string>("");
+  const [weight, setWeight] = useState<string>("");
+  const [color, setColor] = useState<string>("");
+  const [active, setActive] = useState<boolean>(false);
+  const { products } = useContext(ProductsContext);
+  useEffect((): void => {
+    try {
+      const theProduct = products.filter(
+        (product: { ean: string }) => product.ean === id
+      );
+      setName(theProduct[0].name);
+      setEan(theProduct[0].ean);
+      setType(theProduct[0].type);
+      setWeight(theProduct[0].weight);
+      setColor(theProduct[0].color);
+      setActive(theProduct[0].active);
+    } catch {
+      console.log("Could not mount the product");
+    }
+  }, []);
+
   const { id } = useParams<{ id: string }>();
-  return <h1>ProductPage {id}</h1>;
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{ean}</h3>
+      <h3>{type}</h3>
+      <h3>{weight}</h3>
+      <h3>{color}</h3>
+      <h3>{active && "The product is live"}</h3>
+    </>
+  );
 };
 
 export default Product;
